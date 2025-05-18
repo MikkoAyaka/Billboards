eb11b17145e3c68b8ee31e1304a7dfe67ed84bf8
人間工作 <coolxiaom95@gmail.com>
2025-05-18 20:32:39 +0800

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,17 +11,15 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     maven("https://repo.codemc.io/repository/maven-public/")
     maven("https://jitpack.io")
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.helpch.at/releases/")
     maven("https://repo.rosewooddev.io/repository/public/")
-    maven("https://repo.dmulloy2.net/repository/releases/")
-    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     compileOnly("net.milkbowl.vault:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("com.github.dmulloy2:ProtocolLib:5.2.0")
 
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("commons-lang:commons-lang:2.6")
